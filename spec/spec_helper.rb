6f9b909e2e530beb0e74ae358d928be61020c3ad
Patrick Robinson <patrick.robinson@envato.com>
2016-01-18 11:36:43 +1100

    Refactor Request class
    Instead of monkey patching rest-client, we can use it's public methods
    and implement our own Get using net/http

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,20 +2,17 @@ require 'rspec'
 require 'webmock/rspec'
 require 'akamai_rspec'
 
-DOMAIN = 'www.example.com.edgesuite.net'
-RestClient::Request.prod_domain(DOMAIN)
-
 def stub_headers(url, headers, body = 'abc')
-  stub_request(:any, DOMAIN + url).to_return(
+  stub_request(:any, url).to_return(
     body: body, headers: headers)
 end
 
 def stub_status(url, status)
-  stub_request(:any, DOMAIN + url).to_return(
+  stub_request(:any, url).to_return(
     body: 'abc', status: [status, 'message'])
 end
 
 def stub_redirect(status, location = '/redirected')
-  stub_request(:any, DOMAIN + '/redirect').to_return(
-    body: 'abc', headers: { 'Location' => DOMAIN + location }, status: [status, 'message'])
+  stub_request(:any, '/redirect').to_return(
+    body: 'abc', headers: { 'Location' => location }, status: [status, 'message'])
 end
