df4ec25a7209bd0bb93a49169cd36bc6dfb83443
Patrick Robinson <patrick.robinson@envato.com>
2016-01-18 16:34:56 +1100

    Allow spec_helper to prepend domain
    Instead of setting it in a Global manner, which prevents over-writing it
    in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,16 +2,13 @@ require 'rspec'
 require 'webmock/rspec'
 require 'akamai_rspec'
 
-DOMAIN = 'www.example.com.edgesuite.net'
-AkamaiRSpec::Request.prod_domain = DOMAIN
-
-def stub_headers(url, headers, body = 'abc')
-  stub_request(:any, DOMAIN + url).to_return(
+def stub_headers(domain, url, headers, body = 'abc')
+  stub_request(:any, "#{domain}/#{url}").to_return(
     body: body, headers: headers)
 end
 
-def stub_status(url, status)
-  stub_request(:any, DOMAIN + url).to_return(
+def stub_status(domain, url, status)
+  stub_request(:any, "#{domain}/#{url}").to_return(
     body: 'abc', status: [status, 'message'])
 end
 
