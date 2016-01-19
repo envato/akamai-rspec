f6df68642c33728e49aa05447f65df5d59a77734
Patrick Robinson <patrick.robinson@envato.com>
2016-01-19 14:42:21 +1100

    Fix specs

--- spec/functional/x_cache_headers_spec.rb
+++ spec/functional/x_cache_headers_spec.rb
@@ -1,60 +1,65 @@
 require 'spec_helper'
 
 describe 'have_cp_code_set' do
+  let(:prod_domain) { 'www.example.com.edgesuite.net' }
   before(:each) do
-    stub_headers('/correct', 'x-cache-key' => 'cp-code')
-    stub_headers('/correct-true-cache-key', 'x-true-cache-key' => 'cp-code')
-    stub_headers('/no-cp', {})
+    AkamaiRSpec::Request.prod_domain = prod_domain
+    stub_headers(prod_domain, 'correct', 'x-cache-key' => 'cp-code')
+    stub_headers(prod_domain, 'correct-true-cache-key', 'x-true-cache-key' => 'cp-code')
+    stub_headers(prod_domain, 'no-cp', {})
   end
 
   it 'should succeed when cp code set in x-cache-key' do
-    expect(DOMAIN + '/correct').to have_cp_code('cp-code')
+    expect('example.com/correct').to have_cp_code('cp-code')
   end
 
   it 'should succeed when cp code set in x-true-cache-key' do
-    expect(DOMAIN + '/correct-true-cache-key').to have_cp_code('cp-code')
+    expect('example.com/correct-true-cache-key').to have_cp_code('cp-code')
   end
 
   it 'should fail when cp code is wrong' do
-    expect { expect(DOMAIN + '/correct').to have_cp_code('wrong') }.to raise_error(RSpec::Expectations::ExpectationNotMetError)
+    expect { expect('example.com/correct').to have_cp_code('wrong') }.to raise_error(RSpec::Expectations::ExpectationNotMetError)
   end
 
   it 'should fail when both cache-key headers are not set' do
-    expect { expect(DOMAIN + '/no-cp').to have_cp_code('wrong') }.to raise_error(RSpec::Expectations::ExpectationNotMetError)
+    expect { expect('example.com/no-cp').to have_cp_code('wrong') }.to raise_error(RSpec::Expectations::ExpectationNotMetError)
   end
 end
 
 describe 'be_served_from_origin' do
+  let(:prod_domain) { 'www.example.com.edgesuite.net' }
+
   before(:each) do
+    AkamaiRSpec::Request.prod_domain = prod_domain
     x_cache = { 'x-cache-key' => 'A/B/1234/123456/000/originsite.example.com/' }
     x_true_cache = { 'x-true-cache-key' => 'A/B/1234/123456/000/originsite.example.com/' }
-    stub_headers('/correct', x_cache)
-    stub_headers('/correct-true', x_true_cache)
-    stub_request(:any, DOMAIN + '/redirect').to_return(
+    stub_headers(prod_domain, 'correct', x_cache)
+    stub_headers(prod_domain, 'correct-true', x_true_cache)
+    stub_request(:any, "#{prod_domain}/redirect").to_return(
       body: 'abc', headers: x_cache,
       status: [300, 'message'])
   end
 
   it 'should succeed with 200 and correct origin in x-cache-key' do
-    expect(DOMAIN + '/correct').to be_served_from_origin('originsite.example.com')
+    expect('example.com/correct').to be_served_from_origin('originsite.example.com')
   end
 
   it 'should succeed with 200 and correct origin in x-true-cache-key' do
-    expect(DOMAIN + '/correct-true').to be_served_from_origin('originsite.example.com')
+    expect('example.com/correct-true').to be_served_from_origin('originsite.example.com')
   end
 
   it 'should fail on 300 and correct origin' do
-    expect { expect(DOMAIN + '/redirect').to be_served_from_origin('originsite.example.com') }
+    expect { expect('example.com/redirect').to be_served_from_origin('originsite.example.com') }
       .to raise_error(RSpec::Expectations::ExpectationNotMetError)
   end
 
   it 'should fail on 200 and incorrect origin' do
-    expect { expect(DOMAIN + '/correct').to be_served_from_origin('someothersite.example.com') }
+    expect { expect('example.com/correct').to be_served_from_origin('someothersite.example.com') }
       .to raise_error(RSpec::Expectations::ExpectationNotMetError)
   end
 
   it 'should fail on 200 and origin that only partially matches' do
-    expect { expect(DOMAIN + '/correct').to be_served_from_origin('site.example.com') }
+    expect { expect('example.com/correct').to be_served_from_origin('site.example.com') }
       .to raise_error(RSpec::Expectations::ExpectationNotMetError)
   end
 end
