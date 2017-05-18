73de88ca845b78ac38e20c3a6cf637e6fabdd523
Ross Simpson <ross.simpson@envato.com>
2017-05-18 15:22:38 +1000

    Add more debug headers
    Also make the list more readable.

--- lib/akamai_rspec/akamai_headers.rb
+++ lib/akamai_rspec/akamai_headers.rb
@@ -1,7 +1,20 @@
 module AkamaiHeaders
   def self.akamai_debug_headers
     {
-      pragma: 'akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-extracted-values, akamai-x-get-nonces, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-serial-no'
+      pragma: %w(
+        akamai-x-cache-on
+        akamai-x-cache-remote-on
+        akamai-x-check-cacheable
+        akamai-x-feo-trace
+        akamai-x-get-cache-key
+        akamai-x-get-client-ip
+        akamai-x-get-extracted-values
+        akamai-x-get-nonces
+        akamai-x-get-request-id
+        akamai-x-get-ssl-client-session-id
+        akamai-x-get-true-cache-key
+        akamai-x-serial-no
+      ).join(', ')
     }
   end
 end
